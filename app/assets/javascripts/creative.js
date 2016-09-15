471cf9db70d75d0cc521a7b099ab0d7307f175cf
CristianChris <cristian.cartofeanu@gmail.com>
2016-09-16 00:31:36 +0200

    theme succesfully installed

--- app/assets/javascripts/creative.js
+++ app/assets/javascripts/creative.js
@@ -33,7 +33,7 @@ $(document).ready(function(){
       $("h1").fitText(
           1.2, {
               minFontSize: '35px',
-              maxFontSize: '65px'
+              maxFontSize: '50px'
           }
       );
 
